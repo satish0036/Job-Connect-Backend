e9b7275a6de7502af6255f55a7b42d8b7de9cd40
satish0036 <satish0036kumar@gmail.com>
2023-08-01 02:15:28 +0530

    change3

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import mysql, { createConnection } from "mysql";
 import dotenv from 'dotenv';
 import express from "express";
 import router from "./routes/Auth.js";
@@ -7,17 +6,20 @@ import cors from "cors"
 import cookieParser from "cookie-parser";
 const PORT=process.env.PORT || 8800;
 const app=express();
-app.use(cookieParser())
-app.use(express.json())
 // app.use(cors({
 //     origin: "http://localhost:3000",
 //     credentials: true,
 //   }));
+dotenv.config();
 
 app.use(cors({
   origin: '*',
   credentials: true,
 }));
+
+app.use(cookieParser())
+app.use(express.json())
+
 
 app.use("/api/auth",router)
 app.use("/api/post",postRouter)
