dd828065a7a29f84eaa4eb7f8b436ffe3ff3c7a0
satish0036 <satish0036kumar@gmail.com>
2023-08-01 01:39:31 +0530

    change2

--- index.js
+++ index.js
@@ -9,11 +9,15 @@ const PORT=process.env.PORT || 8800;
 const app=express();
 app.use(cookieParser())
 app.use(express.json())
-app.use(cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  }));
+// app.use(cors({
+//     origin: "http://localhost:3000",
+//     credentials: true,
+//   }));
 
+app.use(cors({
+  origin: '*',
+  credentials: true,
+}));
 
 app.use("/api/auth",router)
 app.use("/api/post",postRouter)
